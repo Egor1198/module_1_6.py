a829649ee6def699e541afc7f440a93d4e9041b2
Egor1198 <belosokhovegor@gmail.com>
2024-08-05 23:02:07 +0300

    Add files via upload

---
+++ module_1_6.py
@@ -0,0 +1,16 @@
+# my_dict = {'Timofey':2017, 'Marya':2021, 'Elena':1990}
+# print(my_dict)
+# print(my_dict['Timofey'])
+# print(my_dict.get('Lev','Отсутствет'))
+# my_dict.update({'Aleksander':1959,
+#                'Mariya':1961})
+# my_dict.pop ('Aleksander')
+# print(my_dict)
+
+
+my_set = {1,2,3,3,2,1,'Apple', 'Milk', False,(1,2,3,'Apple')}
+print(my_set)
+up = [222,333,444]
+my_set.update(up)
+my_set.remove(222)
+print(my_set)
